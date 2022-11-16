2cc9c03d98d10b8d77232d89c5ccc366609cb90c
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2022-11-16 17:14:29 +0900

    Support downloading private videos
    Add support downloading private videos for `VideoDownloader`

--- utils/video_downloader.py
+++ utils/video_downloader.py
@@ -2,32 +2,64 @@ from __future__ import annotations
 
 import multiprocessing
 import multiprocessing.dummy
-from typing import Any, Callable
+from typing import Any, Callable, Optional
 
 import youtube_dl
-from youtube_dl.utils import YoutubeDLError
+from playwright.sync_api import sync_playwright
+from youtube_dl.utils import DownloadError, YoutubeDLError
+
+from .twitter_crawler import TwitterCrawler
 
 
 class VideoDownloader:
+    failed_videos: list[str] = []
 
-    def _execute_parallel(self, func: Callable[[str], None], *args: Any, **kwargs: Any) -> None:
-        pool = multiprocessing.dummy.Pool(multiprocessing.cpu_count())
-        pool.map(func, *args, **kwargs)
+    def download_videos(
+        self,
+        links: list[str],
+        username: Optional[str] = None,
+        password: Optional[str] = None,
+    ) -> None:
+        arguments = [(link, username, password) for link in links]
+        self._execute_parallel(
+            self.download_video,
+            arguments,
+        )
 
-    def download_video(self, link: str) -> None:
-        ydl_opt = {
-            'format': 'bestvideo/best',
-            'outtmpl': 'videos/%(title)s.%(ext)s',
-        }
+    def download_video(self, link: str, username: Optional[str] = None, password: Optional[str] = None) -> None:
         try:
-            with youtube_dl.YoutubeDL(ydl_opt) as youtube_dl_downloader:
-                youtube_dl_downloader.download([link])
+            self._download_public_video(link)
+        except DownloadError as error:
+            if not username or not password:
+                raise ValueError('Username and password are required for private videos') from error
+            print('Failed to download video, trying to download with authentication ...')
+            self._download_twitter_private_video(link, username, password)
+            self.failed_videos.append(link)
         except YoutubeDLError:
             link = link.replace('\n', '')
             print(f'{link} failed')
 
-    def download_videos(self, links: list[str]) -> None:
-        self._execute_parallel(self.download_video, links)
+    def _download_public_video(self, link: str, video_filename: Optional[str] = None) -> None:
+        youtube_dl_output_template = f'videos/{video_filename}' if video_filename else 'videos/%(title)s.%(ext)s'
+        youtube_dl_option = {'format': 'bestvideo/best', 'outtmpl': youtube_dl_output_template}
+        with youtube_dl.YoutubeDL(youtube_dl_option) as youtube_dl_downloader:
+            youtube_dl_downloader.download([link])
 
+    def _download_twitter_private_video(self, target_link: str, username: str, password: str) -> None:
+        with sync_playwright() as playwright_sync:
+            browser = playwright_sync.webkit.launch(headless=True)
+            page = browser.new_page()
+            crawler = TwitterCrawler(page)
+            crawler.login(username, password)
+            video_info = crawler.get_video_of_tweet(target_link)
+            if video_info:    # only if video exists
+                print('twitter video found, downloading ...')
+                video_filename, links = video_info
+                for link in links:
+                    self._download_public_video(link, video_filename)
+            page.close()
+            browser.close()
 
-video_downloader = VideoDownloader()
+    def _execute_parallel(self, func: Callable[[str], None], *args: Any, **kwargs: Any) -> None:
+        pool = multiprocessing.dummy.Pool(multiprocessing.cpu_count())
+        pool.starmap(func, *args, **kwargs)
