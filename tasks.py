98a8fb7dbe2c2db30b6c39c05043bfa190568f4e
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2022-11-17 14:53:01 +0900

    Version up

--- tasks.py
+++ tasks.py
@@ -7,3 +7,9 @@ import monkey_patch_invoke as _
 def test(context: Context) -> None:
     """Run tests."""
     context.run('pytest . ', pty=True)
+
+
+@task
+def publish(context: Context) -> None:
+    """Publish to PyPI."""
+    context.run('poetry publish --build', pty=True)
