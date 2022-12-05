283e9968e741978574ec4b2103edc26c4acc0bc7
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2022-12-05 21:01:00 +0900

    Add class `TwitterVideoTools` as a interface

--- twitter_video_tools/__init__.py
+++ twitter_video_tools/__init__.py
@@ -4,4 +4,5 @@ from .monsnode_parser import MonsnodeParser as MonsnodeParser
 from .platform_video_downloader import \
     PlatformVideoDownloader as PlatformVideoDownloader
 from .twitter_crawler import TwitterCrawler as TwitterCrawler
+from .twitter_video_tools import TwitterVideoTools as TwitterVideoTools
 from .video_downloader import VideoDownloader as VideoDownloader
