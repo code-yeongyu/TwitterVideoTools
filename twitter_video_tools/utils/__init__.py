0c5b61447aa3ebdd1691bbbeeade0b4803458dd2
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2022-12-05 22:30:35 +0900

    Add util url validator

--- twitter_video_tools/utils/__init__.py
+++ twitter_video_tools/utils/__init__.py
@@ -1,4 +1,5 @@
 # pylint: disable=useless-import-alias
 
 from .execute_parallel import execute_parallel as execute_parallel
+from .url_validator import URLValidator as URLValidator
 from .youtube_dl_wrapper import youtube_dl_wrapper as youtube_dl_wrapper
